7acc45c2c23768708b3044ca7fb3fca4e87966fc
KrystofBeh <kacko896ytb@gmail.com>
2026-05-04 07:35:36 +0200

    nevim co se tady stalo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,17 +3,32 @@ import {useEffect, useState} from "react";
 function Login() {
 
 
-    const [user, setUser] = useState([]);
+    // const [user, setUser] = useState([]);
+    //
+    // useEffect(() => {fetch("http://localhost:8080/returnUsers")
+    //     .then(res => res.json())
+    //     .then(data => setUser(data));},[]);
 
-    useEffect(() => {fetch("http://localhost:8080/returnUsers")
-        .then(res => res.json())
-        .then(data => setUser(data));},[]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const convertedData = Object.fromEntries(formData.entries());
+
+        fetch('http://localhost:8080/loginCheck', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(convertedData)
+        })
+    }
+
 
 
 
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Jméno</label>
                 <input type="text" id="name" name="name"/>
