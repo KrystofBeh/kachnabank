e85cf7ff5e332190d737c421ef7d5476d9edcdb2
KrystofBeh <kacko896ytb@gmail.com>
2026-05-26 10:12:56 +0200

    Detaily uctu skoro done

---
+++ src/pages/AccountDetails.js
@@ -0,0 +1,7 @@
+function AccountDetails(){
+    return (
+        <div></div>
+    )
+}
+
+export default AccountDetails
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,58 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 function Home(){
 
     const [data, setData] = useState("Nacita");
     const navigate = useNavigate();
+    const [accounts, setAccounts] = useState([]);
+    const userId = localStorage.getItem("userId");
 
     fetch("http://localhost:8080/get")
         .then(res => res.text())
         .then(data => setData(data))
         .catch(err => setData("Chyba"));
 
+    useEffect(() => {
+        async function loadAccounts() {
+            try {
+                const response = await fetch(`http://localhost:8080/getAccounts/${userId}`);
 
+                if (!response.ok) {
+                    throw new Error("Chyba při načítání účtů");
+                }
+
+                const data = await response.json();
+                setAccounts(data);
+
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        if (userId) {
+            loadAccounts();
+        }
+    }, [userId]);
 
     return (
         <div>
-            <button id={"CreateAcc"} onClick={() => navigate("/createAcc")}>Vytvorit acc</button>
+            <button onClick={() => navigate("/createAcc")}>
+                Vytvorit acc
+            </button>
+
             <h1>{data}</h1>
+
+            <ul>
+                {accounts.map((acc) => (
+                    <li key={acc.id}>
+                        {acc.name}
+                        <button onClick={() =>  navigate(`/acc/${userId}/${acc.id}`)}>Details</button>
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
 
 export default Home;
